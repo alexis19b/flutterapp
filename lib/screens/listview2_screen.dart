a125b5244b7382e6deda0708d57c8fa809201392
Jesus Alexis <ingjesusalexis@gmail.com>
2023-03-09 21:36:52 -0400

    listView2 added

---
+++ lib/screens/listview2_screen.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class Listview2Screen extends StatelessWidget {
+  const Listview2Screen({Key? key}) : super(key: key);
+
+  final options = const [
+    'fifa2022',
+    'crash car',
+    'need for speed',
+    'mario bros'
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('ListView tipo 2'),
+        ),
+        body: ListView.separated(
+            itemBuilder: (context, index) => ListTile(
+                  title: Text(options[index]),
+                  trailing: const Icon(Icons.arrow_back_ios_outlined),
+                ),
+            separatorBuilder: (_, __) => const Divider(),
+            itemCount: options.length));
+  }
+}

af7b5cb924846e13ea78f93e71232d8f10467d42
Jesus Alexis <ingjesusalexis@gmail.com>
2023-03-18 10:17:41 -0400

    adeed card2

--- lib/widgets/custom_car_type2.dart
+++ lib/widgets/custom_car_type2.dart
@@ -1,3 +1,4 @@
+import 'package:fl_components/theme/app_theme.dart';
 import 'package:flutter/material.dart';
 
 class CustomCardType2 extends StatelessWidget {
@@ -6,13 +7,24 @@ class CustomCardType2 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Card(
-      elevation: 10,
+      clipBehavior: Clip.antiAlias,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
+      elevation: 15,
+      shadowColor: AppTheme.primary.withOpacity(0.5),
       child: Column(
-        children: const [
-          FadeInImage(
-              image: NetworkImage(
-                  'https://tecnovortex.com/wp-content/uploads/2019/04/wallpaper-engine.jpg'),
-              placeholder: AssetImage('assets/jar-loading.gif'))
+        children: [
+          const FadeInImage(
+            image: NetworkImage(
+                'https://tecnovortex.com/wp-content/uploads/2019/04/wallpaper-engine.jpg'),
+            placeholder: AssetImage('assets/jar-loading.gif'),
+            width: double.infinity,
+            height: 230,
+            fit: BoxFit.cover,
+          ),
+          Container(
+              alignment: AlignmentDirectional.centerEnd,
+              padding: const EdgeInsets.only(right: 20, top: 10, bottom: 10),
+              child: const Text('Hola mundo')),
         ],
       ),
     );
